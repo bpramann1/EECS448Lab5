cd26e41dcc2b5ce3109fa78f5c560596672a19ab
Brandon Pramann <bpramann1@gmail.com>
2019-03-31 21:01:21 -0500

    Design CreatePosts

--- CreatePosts.php
+++ CreatePosts.php
@@ -1,8 +1,33 @@
 <?php
+
+echo "<html>
+<head>
+    <link href='MyCustomStyle.css' rel='stylesheet' type='text/css' />
+    <title>Create User</title>
+</head>
+<body>
+    <header>
+        <div class='Container'>
+            <div class='Navbar'>
+                <h1 id='NavExp'>Pages</h1>
+            </div>
+            <div class='Navbar'>
+                <a href='https://people.eecs.ku.edu/~b122p496/subdir/eecs448/Lab05/UserHome.html'>User Home</a>
+            </div>
+			<div class='Navbar'>
+                <a href='https://people.eecs.ku.edu/~b122p496/subdir/eecs448/Lab05/CreateUser.html'>Create User</a>
+            </div>
+            <div class='Navbar'>
+                <a href='https://people.eecs.ku.edu/~b122p496/subdir/eecs448/Lab05/CreatePosts.html'>Create posts</a>
+            </div>
+        </div>
+    </header>";
+
 //access the global array called $_POST to get the values from the text fields
 $username = $_POST["username"];
+$content = $_POST["message"];
 if ($username != "") {
-	echo "<p>Attempted to add username: " . $username . "<br></p>";
+	echo "<p>Attempting to use username: " . $username . "<br></p>";
 	$myDatabaseClass = new mysqli("mysql.eecs.ku.edu", "b122p496", "yae9Kei3", "b122p496");
 
 	/* check connection */
@@ -28,15 +53,21 @@ if ($username != "") {
 
 
 		if ($user_exists){
-			echo "<p>Username already exists! Choose another!</p>";
-		}else{
-			$query = "insert into Users (user_id) values ( '" . $username . "');";
-			If($myDatabaseClass->query($query)){
-				echo "<p>Username added!</p>";
+			echo "<p>Attempting to post the message with content: " . $content . "<br></p>";
+			if ($content=="" ) {
+				echo "<p>Message is empty! Please enter text into the message and retry submission!</p>";
 			}else{
-				echo "<p>Failed adding username!</p>";
+				$query = "insert into Posts (content, author_id) values ( '" . $content . "', '" . $username . "');";
+				If($myDatabaseClass->query($query)){
+					echo "<p>Message Posted!</p>";
+				}else{
+					echo "<p>Failed posting message!</p>";
+				}
 			}
 
+
+		}else{
+			echo "<p>Enter a valid username!</p>";
 		}
 	}
 
@@ -44,14 +75,15 @@ if ($username != "") {
 	$myDatabaseClass->close();
 }
 else {
-	echo "<p>Username must be non-empty!</p>";
+	echo "<p>You must enter a username in order to post a message!</p>";
 }
 
-
-
-
-
-
-
+echo "    <footer>
+        <p>
+            Created by Brandon Pramann 2019.
+        </p>
+    </footer>
+</body>
+</html>";
 
 ?> 
